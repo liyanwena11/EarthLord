369f190bb3bf9224f063820671ff43c7f0875212
liyanwen <lyanwen423@gmail.com>
2025-12-30 20:30:46 +0800

    Add developer signature

--- EarthLord/ContentView.swift
+++ EarthLord/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+
+            Spacer()
+                .frame(height: 30)
+
+            Text("Developed by lyanwen")
+                .font(.headline)
+                .foregroundColor(.blue)
         }
         .padding()
     }
